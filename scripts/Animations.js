e1ad5a43f8ac346a5a7a11567e8fbafa5209e051
PAUL STROOT <paul@ocupop.com>
2024-10-22 16:27:20 -0500

    Comment out Lottie animation

--- scripts/Animations.js
+++ scripts/Animations.js
@@ -1,11 +1,12 @@
-import Lottie from 'lottie-react';
+// import Lottie from 'lottie-react';
 import React from 'react';
-import animation from './lottie/how-it-works-animation.json';
+// import animation from './lottie/how-it-works-animation.json';
 
 export function HowItWorksAnimation() {
   return (
     <>
-      <Lottie animationData={animation} loop={true} />
+      LOTTIE ANIMATION...
+      {/* <Lottie animationData={animation} loop={true} /> */}
     </>
   );
 }
