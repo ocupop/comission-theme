edcf3be4aa8b34965ac3eff086103c023836c2ad
PAUL STROOT <paul@ocupop.com>
2024-10-29 09:04:53 -0500

    theme template updates for jobs and faq

--- scripts/checkout-disclaimer.js
+++ scripts/checkout-disclaimer.js
@@ -3,13 +3,11 @@ const submitButtons = document.querySelectorAll('.requires-disclaimer-input');
 
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener('click', function () {
-    console.log('Clicked ', this.classList);
     setSubmitDiabledState(this.checked);
   });
 });
 
 function setSubmitDiabledState(state) {
-  console.log('Set State To ', state);
   checkboxes.forEach((checkbox) => {
     checkbox.checked = state;
   });
@@ -20,3 +18,17 @@ function setSubmitDiabledState(state) {
 // check initial state of checkbox (sometimes is defaulst to checked if the user uses the browser back button)
 
 setSubmitDiabledState(checkboxes[0].checked);
+
+const modalLinks = document.querySelectorAll('.modal-link');
+
+modalLinks.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    window.open(
+      href,
+      'policyWindow',
+      'location=no,width=600,height=600,scrollbars=yes,top=100,left=700,resizable = no'
+    );
+  });
+});
